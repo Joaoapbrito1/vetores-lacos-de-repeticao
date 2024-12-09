a9a38862bd8dec270354351fd591be21faf5fc65
Joaoapbrito1 <joao.brito@zup.com.br>
2024-12-09 12:22:09 -0300

    Feat: Implementa o laço de repetição (For).

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero:");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite o segundo numero:");
+        int numero2 = scanner.nextInt();
+        System.out.println("Digite o terceiro numero:");
+        int numero3 = scanner.nextInt();
+        System.out.println("Digite o quarto numero:");
+        int numero4 = scanner.nextInt();
+        System.out.println("Digite o quinto numero:");
+        int numero5 = scanner.nextInt();
+
+        int[] vetor = {numero1, numero2, numero3, numero4, numero5};
+
+        System.out.println("Imprimindo com For:");
+        for (int index = 0; index < vetor.length; index++) {
+            System.out.println(vetor[index]);
+        }
     }
 }
